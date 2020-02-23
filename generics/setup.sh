947471cd5bbaf143b61e92f4c546c782646e1f60
Michael Cotterell <mepcotterell@gmail.com>
2020-02-23 13:13:49 -0500

    Update setup.sh

--- generics/setup.sh
+++ generics/setup.sh
@@ -9,11 +9,13 @@ if [ ! -d "cs1302-demos-generics" ]; then
   mv generics/* ./
   rm -rf generics
   rm -rf .git
+  rm -f src/cs1302/generics/Container.java
   mkdir bin doc
   git init
   git add README.md .gitignore src 
   git commit -m "initial commit"
   echo "subdirectory cs1302-demos-generics successfully created"
+  echo " - src/cs1302/generics/Container.java intentionally not included
 else
   >&2 echo "subdirectory cs1302-demos-generics already exists"
 fi  
