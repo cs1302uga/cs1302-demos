22124bd307620a40abc927891cced93746efe0d3
Michael Cotterell <mepcotterell@gmail.com>
2020-02-23 13:32:02 -0500

    Create compile.sh

---
+++ generics/compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ex
+
+rm -rf bin/*
+mkdir -p bin
+javac -d bin src/cs1302/Utility.java
+javac -d bin -cp bin src/cs1302/generics/ObjectContainer.java
+javac -d bin -cp bin src/cs1302/generics/Driver.java
+checkstyle -c cs1302_checks.xml src
