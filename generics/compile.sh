a2711ba6012be383be8c5f963d2c1abfdfef3058
Michael Cotterell <mepcotterell@gmail.com>
2020-02-23 13:36:49 -0500

    Update compile.sh

--- generics/compile.sh
+++ generics/compile.sh
@@ -6,3 +6,4 @@ javac -d bin src/cs1302/Utility.java
 javac -d bin -cp bin src/cs1302/generics/ObjectContainer.java
 javac -d bin -cp bin src/cs1302/generics/Driver.java
 checkstyle -c cs1302_checks.xml src
+java -cp bin cs1302.generics.Driver
