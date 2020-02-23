cddd71a6807bbc6e33e3a2a06e005288e33e5cbd
Michael Cotterell <mepcotterell@gmail.com>
2020-02-23 13:14:02 -0500

    Update setup.sh

--- generics/setup.sh
+++ generics/setup.sh
@@ -15,7 +15,7 @@ if [ ! -d "cs1302-demos-generics" ]; then
   git add README.md .gitignore src 
   git commit -m "initial commit"
   echo "subdirectory cs1302-demos-generics successfully created"
-  echo " - src/cs1302/generics/Container.java intentionally not included
+  echo " - src/cs1302/generics/Container.java intentionally not included"
 else
   >&2 echo "subdirectory cs1302-demos-generics already exists"
 fi  
