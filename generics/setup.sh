90ab0ce21ff4fbfd502889a40a57eca828549978
Michael Cotterell <mepcotterell@gmail.com>
2020-02-23 12:58:39 -0500

    Create setup.sh

---
+++ generics/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -d "cs1302-demos-generics" ]; then
+  git clone --depth 1 --no-checkout https://github.com/cs1302uga/cs1302-tutorials.git cs1302-demos
+  cd cs1302-demos
+  git checkout master -- generics
+  rm -f generics/setup.sh
+  mv generics/* ./
+  rm -rf generics
+  rm -rf .git
+  mkdir bin doc
+  git init
+  git add README.md src
+  git commit -m "initial commit"
+  echo "subdirectory cs1302-demos-generics successfully created"
+else
+  >&2 echo "subdirectory cs1302-demos-generics already exists"
+fi  
